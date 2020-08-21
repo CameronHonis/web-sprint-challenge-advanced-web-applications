8fcec67f29f556a9c59020542015fcc94d10cfc3
CameronHonis <cameron-04@hotmail.com>
2020-08-21 15:40:06 -0400

    assignment

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { render, screen, getByTestId, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 import { fetchColors as mockFetchColors } from './fetchColors'
 
 jest.mock('./fetchColors')
 
-const fakeColors = [
-  {
-    color: 'aliceblue',
-    code: {hex: '#f0f8ff'},
-    id: 1,
-  }
-]
+const fakeColors = {
+  data: [
+    {
+      color: 'aliceblue',
+      code: {hex: '#f0f8ff'},
+      id: 1,
+    }
+  ]
+}
 
 test("Fetches data and renders the bubbles", async () => {
   mockFetchColors.mockResolvedValueOnce(fakeColors)
   localStorage.setItem('token', "ahuBHejkJJiMDhmODZhZi0zaeLTQ4ZfeaseOGZgesai1jZWYgrTA07i73Gebhu98")
+  //const { getByTestId } = render(<BubblePage />)
   render(<BubblePage />)
-  await waitFor(() => getByTestId('bubble'))
+  await waitFor(() => {expect(screen.getByTestId(/bubble/i))})
 });
